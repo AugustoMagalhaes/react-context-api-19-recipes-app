b86d079f4e8c2cb9f7bd7100ed8f9425cb2a4a27
AugustoMagalhaes <augustommg@gmail.com>
2022-05-01 06:17:04 -0300

    Criando um componente independente para SearchBar.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,36 +4,14 @@ import { Link } from 'react-router-dom';
 import profile from '../../images/profileIcon.svg';
 import search from '../../images/searchIcon.svg';
 import './Header.css';
-import { getFoodsByIngredient, getFoodsByName, getFoodsByFirsLetter }
-from '../../services/fetchFoods';
+import SearchBar from '../SearchBar.js';
 
 function Header({ pageTitle, hasSearch }) {
   const [displaySearchBar, setDisplaySearchBar] = useState(false);
-  const [searchInput, setSearchInput] = useState('');
-  const [searchRadio, setSearchRadio] = useState('ingredient');
-
-  console.log('search Radio ', searchRadio);
 
   useEffect(() => {
     document.title = pageTitle;
   });
-
-  const getFoods = async () => {
-    /* const refObj = {
-      ingredient: () => getFoodsByIngredient(searchInput),
-      name: async () => getFoodsByName(searchInput),
-      firstLetter: async () => getFoodsByFirsLetter(searchInput),
-    }; */
-    if (searchRadio === 'name') {
-      return getFoodsByName(searchInput);
-    }
-    if (searchRadio === 'ingredient') {
-      return getFoodsByIngredient(searchInput);
-    }
-    if (searchRadio === 'firstLetter') {
-      return getFoodsByFirsLetter(searchInput);
-    }
-  };
 
   const cursorStyle = window.location.pathname === '/profile' ? 'auto' : 'pointer';
 
@@ -53,10 +31,10 @@ function Header({ pageTitle, hasSearch }) {
       <h1 data-testid="page-title">{ pageTitle }</h1>
       {hasSearch
         ? (
-          <>
+          <section>
             <button
               type="button"
-              className="teste"
+              className="search-btn"
               onClick={ () => setDisplaySearchBar(!displaySearchBar) }
             >
               <img
@@ -65,65 +43,8 @@ function Header({ pageTitle, hasSearch }) {
                 data-testid="search-top-btn"
               />
             </button>
-            {
-              displaySearchBar && (
-                <section>
-                  <input
-                    type="text"
-                    data-testid="search-input"
-                    placeholder="Pesquise uma comida ou bebida"
-                    size="30"
-                    value={ searchInput }
-                    onChange={ (e) => setSearchInput(e.target.value) }
-                  />
-                  <label htmlFor="search-radio">
-                    Ingrediente:
-                    <input
-                      type="radio"
-                      name="search-radio"
-                      id="search-radio"
-                      data-testid="ingredient-search-radio"
-                      value="ingredient"
-                      onChange={ (e) => setSearchRadio(e.target.value) }
-                      checked={ searchRadio === 'ingredient' }
-                    />
-                  </label>
-
-                  <label htmlFor="search-radio">
-                    Name:
-                    <input
-                      type="radio"
-                      name="search-radio"
-                      id="search-radio"
-                      data-testid="name-search-radio"
-                      value="name"
-                      onChange={ (e) => setSearchRadio(e.target.value) }
-                    />
-                  </label>
-
-                  <label htmlFor="search-radio">
-                    First Letter:
-                    <input
-                      type="radio"
-                      name="search-radio"
-                      id="search-radio"
-                      data-testid="first-letter-search-radio"
-                      value="firstLetter"
-                      onChange={ (e) => setSearchRadio(e.target.value) }
-                    />
-                  </label>
-
-                  <button
-                    type="button"
-                    data-testid="exec-search-btn"
-                    onClick={ getFoods }
-                  >
-                    Search
-                  </button>
-                </section>
-              )
-            }
-          </>
+            <SearchBar displaySearchBar={ displaySearchBar } />
+          </section>
         ) : null }
 
       {/*
