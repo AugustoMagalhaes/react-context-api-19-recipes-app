0366f420dd387ba4e3f78da1b4db5e5d6de612fa
AugustoMagalhaes <augustommg@gmail.com>
2022-05-07 10:48:58 -0300

    Corrigindo problema do teste de footer por causa do profile.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer/index';
 
@@ -7,6 +7,64 @@ function ProfileScreen() {
   return (
     <div>
       <Header pageTitle={ pageTitle } hasSearch={ false } />
+      <Footer />
+    </div>
+  );
+}
+
+export default ProfileScreen; */
+
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Header from '../../components/Header/index';
+import Footer from '../../components/Footer/index';
+
+function ProfileScreen() {
+  const [getEmail, setGetEmail] = useState({});
+
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      const email = JSON.parse(localStorage.getItem('user'));
+      setGetEmail(email);
+    }
+  }, []);
+
+  const pageTitle = 'Profile';
+  const history = useHistory();
+
+  const logoutBtn = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <div>
+      <Header pageTitle={ pageTitle } hasSearch={ false } />
+      <h3 data-testid="profile-email">{getEmail.email}</h3>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <br />
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <br />
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => logoutBtn() }
+      >
+        Logout
+      </button>
+      <br />
       <Footer />
     </div>
   );
